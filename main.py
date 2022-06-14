db7a9a0bafd44713776c6632d6620c61fbfb4fcb
theintrovertva <theintrovertva@gmail.com>
2022-06-14 13:42:44 +0100

    commit

--- main.py
+++ main.py
@@ -1,74 +1,82 @@
-import random
+import random, sys
 
 print("Welcome to Rock Paper Scissors game!")
 print("............. Have Fun Playing ...........")
 
+print('''Rock, Paper, Scissors Rules:,
+- Rock beats scissors.
+- Paper beats rocks.
+- Scissors beats paper.
+''')
+
 ### Set up variables
-CPUScore = 0
-PlayerScore = 0
-TieScore = 0
-PossibleHands = ['R' == "Rock", 'P' == "Paper", 'S' == "Scissors"]
+Possible_actions = ['R', 'P', 'S']
+CPU_wins = 0
+PlayerWins = 0
+ties = 0
 
-def checkForWinner(player_choice, CPU_choice):
-  if(player_choice == "Rock" and CPU_choice == "Paper"):
-    print("Player (Rock) : CPU (Paper)")
-    print("Sorry you lost :(")
-    return "Cpu"
-  elif(player_choice == "Rock" and CPU_choice == "Scissors"):
-    print("Player (Rock) : CPU (Scissors)")
-    print("Congrats! You have won :)")
-    return "Player"
-  elif(player_choice == "Scissors" and CPU_choice == "Paper"):
-    print("Player (Scissors) : CPU (Paper)")
-    print("Congrats! You win :)")
-    return "Player"
-  elif(player_choice == "Scissors" and CPU_choice == "Rock"):
-    print("Player (Scissors) : CPU (Rock)")
-    print("Sorry, you lost!")
-    return "Cpu"
-  elif(player_choice == "Paper" and CPU_choice == "Rock"):
-    print("Player (Paper) : CPU (Rock)")
-    print("Congrats! You win :)")
-    return "Player"
-  elif(player_choice == "Paper" and CPU_choice == "Scissors"):
-    print("Player (Paper) : CPU (Scissors)")
-    print("Sorry, you lost!")
-    return "Cpu"
-  else:
-    print("It's a tie, play again!")
-    return "Tie"
 
-### Start game loop
-while(PlayerScore != 3 and CPUScore != 3):
-
-  ### Validate input
-  while True:
-    def choose_option(player_choice):
-      player_choice = input("What is your choice? Pick a hand ('R' for Rock, 'P' for Paper, 'S' for Scissors): """)
-      if player_choice in ["R"]:
-        player_choice = 'R'
-      elif player_choice in ["P"]:
-        player_choice = 'P'
-      elif player_choice in ["S"]:
-        player_choice = 'S'
-      else:
-        print("I do not understand! You picked the wrong option. Try again. :)")
-        choose_option()
-        return player_choice
-  
-    ### Generate computer pick
-    CPU_choice = random.choice(PossibleHands)
+def CPU_option():
+    computerMove = random.choice(Possible_actions)
+    if computerMove == 'R':
+        print('R')
+        computerMove ='R'
+    elif computerMove == "P":
+        print('P')
+        computerMove ='P'   
+    elif computerMove == "S":
+        print('S')
+        computerMove ='S'
     
-    ### Print results
-    print("Player: ", player_choice,)
-    print("CPU: ", CPU_choice)
-    result = checkForWinner(player_choice, CPU_choice)
-    if(result == "Player"):
-      PlayerScore += 1
-    elif(result == "CPU"):
-      CPUScore += 1
+while True:
+    playerMove = input("What is your choice? Pick a hand ('R', 'P', 'S' or Q) '>'")
+    if playerMove == 'Q':
+        print('Thanks for playing!')
+        sys.exit()
+    elif playerMove == 'R':
+        playerMove ='R'
+    elif playerMove == 'P':
+        playerMove ='P'
+    elif playerMove == 'S':
+        playerMove ='S' 
     else:
-      TieScore += 1
-    print("Your score: ", PlayerScore, "CPU: ", CPUScore, "Ties: ", TieScore)
+        print("I do not understand! You picked the wrong option. Type one of R, P, S, or Q. :)") 
 
-print("Game over! Thank you for playing :)")
+    computerMove = random.choice(Possible_actions)
+    print(f"\n Player Move {playerMove} : CPU Move {computerMove}. \n")
+
+    # Display and record the win/loss/tie:
+    if playerMove == computerMove:
+        print(f"Both players selected {playerMove}. It\'s a tie! Play again")
+        PlayerWins = PlayerWins + 0
+        CPU_wins = CPU_wins + 0
+    elif playerMove == 'R' and computerMove == 'S':
+        print('Congrats, You win! :)')
+        PlayerWins = PlayerWins + 1
+    elif playerMove == 'P' and computerMove == 'R':
+        print('Congrats, You win! :)')
+        PlayerWins = PlayerWins + 1
+    elif playerMove == 'S' and computerMove == 'P':
+        print('Congrats, You win!:)')
+        PlayerWins = PlayerWins + 1
+    elif playerMove == 'R' and computerMove == 'P':
+        print('Sorry, You lost! :(')
+        CPU_wins = CPU_wins + 1
+    elif playerMove == 'P' and computerMove == 'S':
+        print('Sorry, You lost! :(')
+        CPU_wins = CPU_wins + 1
+    elif playerMove == 'S' and computerMove == 'R':
+        print('Sorry, You lost! :(')
+        CPU_wins = CPU_wins + 1
+
+    print("")
+    print("Player wins: " + str(PlayerWins))
+    print("Computer wins: " + str(CPU_wins))
+    print("")
+
+    playermove= input("Do you want to play again? (y/n)")
+    if playermove in ["Y", "y", "yes", "Yes"]:
+        pass
+    elif playermove in ["N", "n", "no", "No"]:
+        print("Game over! Thank you for playing :)")
+        sys.exit()
