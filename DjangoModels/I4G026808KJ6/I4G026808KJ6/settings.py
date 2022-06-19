281c2baa77fc6372663892e83f8e37d4ce36f099
theintrovertva <theintrovertva@gmail.com>
2022-06-19 06:37:41 +0100

    commit

--- DjangoModels/I4G026808KJ6/I4G026808KJ6/settings.py
+++ DjangoModels/I4G026808KJ6/I4G026808KJ6/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog'
 ]
 
 MIDDLEWARE = [
@@ -47,7 +48,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'blog'
+    
+
 ]
 
 ROOT_URLCONF = 'I4G026808KJ6.urls'
